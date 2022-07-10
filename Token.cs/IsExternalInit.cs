1c473f0b8e07bf9afb83aed83b3800a0e3914d48
Zander Baker <zanderbaker100@gmail.com>
2022-07-09 17:54:21 -0700

    Add .NET Standard 2.0 target
    [Project Modification]: Add .NET Standard 2.0 target
    
    [File/Class Addition]: Create IsExternalInit (MSBuild bug)

---
+++ Token.cs/IsExternalInit.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices
+{
+    // Le bug: https://stackoverflow.com/a/62656145/16324801
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    // ReSharper disable once UnusedType.Global
+    internal record IsExternalInit;
+}
