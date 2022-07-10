1dcef5ffee767b29fe6b1e426978a9a87c10b9dd
Zander Baker <zanderbaker100@gmail.com>
2022-07-09 17:43:32 -0700

    Add extension methods
    [File/Class Addition]: Create LexerTokenExtensions
    
    [Test Addition]: Create ExtensionTests

---
+++ Token.cs.Tests/LexerTests/ExtensionTests.cs
@@ -0,0 +1,71 @@
+﻿using TokenCs.Extensions;
+
+namespace TokenCs.Tests.LexerTests;
+
+[TestFixture]
+public class ExtensionTests
+{
+    [Test]
+    public void TestAlphabetic()
+    {
+        var token = LexHelper.GetTokens("abc")[0];
+        
+        Assert.That(token.IsAlphabetic);
+    }
+    
+    [Test]
+    public void TestNotAlphabetic()
+    {
+        var token = LexHelper.GetTokens("abc123")[0];
+        
+        Assert.That(!token.IsAlphabetic());
+    }
+    
+    [Test]
+    public void TestAlphaNumeric()
+    {
+        var token = LexHelper.GetTokens("abc123")[0];
+        
+        Assert.That(token.IsAlphaNumeric);
+    }
+    
+    [Test]
+    public void TestNotAlphaNumeric()
+    {
+        var token = LexHelper.GetTokens("123")[0];
+        
+        Assert.That(!token.IsAlphaNumeric());
+    }
+
+    [Test]
+    public void TestNewLineCr()
+    {
+        var token = LexHelper.GetTokens("\r")[0];
+        
+        Assert.That(token.IsNewLine);
+    }
+    
+    [Test]
+    public void TestNewLineLf()
+    {
+        var token = LexHelper.GetTokens("\n")[0];
+        
+        Assert.That(token.IsNewLine);
+    }
+    
+    [Test]
+    public void TestNewLineCrlf()
+    {
+        var token = LexHelper.GetTokens("\r\n")[0];
+        
+        Assert.That(token.IsNewLine);
+    }
+    
+    [Test]
+    public void TestNotNewLineCr()
+    {
+        var token = LexHelper.GetTokens("foo")[0];
+        
+        Assert.That(!token.IsNewLine());
+    }
+}
---
+++ Token.cs/Extensions/LexerTokenExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace TokenCs.Extensions;
+
+public static class LexerTokenExtensions
+{
+    public static bool IsAlphabetic(this LexerToken token)
+    {
+        return token.Tokens.All(c => c.Type == TokenType.Alphabetic);
+    }
+    
+    public static bool IsAlphaNumeric(this LexerToken token)
+    {
+        return token.Tokens.Any(t => t.Type is TokenType.Alphabetic) &&
+               token.Tokens.All(c => c.Type is TokenType.Alphabetic or TokenType.Numeric);
+    }
+
+    public static bool IsNewLine(this LexerToken token)
+    {
+        return token.Type is 
+            LexerTokenType.CarriageReturn or 
+            LexerTokenType.LineFeed or 
+            LexerTokenType.CarriageReturnLineFeed;
+    }
+}
